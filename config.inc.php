7e5256ec32ab5d8f46c3a1024c0daa6b44a6aba4
unknown <John@Coruscant.(none)>
2011-12-08 23:26:23 -0500

    Updated Config to check for dependancies

--- config.inc.php
+++ config.inc.php
@@ -1,4 +1,14 @@
 <?php
-    define('AUTHNET_LOGIN', 'cnpdev4289');
-    define('AUTHNET_TRANSKEY', 'SR2P8g4jdEn7vFLQ');
+    define('AUTHNET_LOGIN', '');
+    define('AUTHNET_TRANSKEY', '');
+
+    if (!function_exists('curl_init'))
+    {
+        throw new Exception('CURL PHP extension not installed');
+    }
+
+    if (!function_exists('simplexml_load_file'))
+    {
+      throw new Exception('SimpleXML PHP extension not installed');
+    }
 ?>
